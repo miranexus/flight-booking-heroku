50132204c62877a25af21c13f00cccab1dd7f676
miranexus <fionnrodgers@gmail.com>
2026-02-19 22:14:13 +0100

    Add debug logging and --save-dev to npm install

--- heroku-build.sh
+++ heroku-build.sh
@@ -2,8 +2,11 @@
 set -e
 (
   cd flight-booking-master
-  echo "Installing dependencies..."
+  echo "Installing dependencies with dev packages..."
+  npm install --legacy-peer-deps --save-dev
   npm install --legacy-peer-deps
+  echo "Listing ng binary location..."
+  ls -la node_modules/.bin/ | grep ng
   echo "Running build..."
   ./node_modules/.bin/ng build --configuration production
   echo "Build complete!"
